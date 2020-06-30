bda1bb5baf0f020763368b3505ae999ca95cb3d4
André de Lima Machado <4ndre.machado@gmail.com>
2020-06-30 16:38:26 -0300

    created endpoint of making bank deposits

--- routes/BankRouter.js
+++ routes/BankRouter.js
@@ -9,11 +9,30 @@ const appRouter = express();
  */
 appRouter.get('/query/:agency/:account', async (req, res) => {
   try {
-    const account = await accountModel.find({
-      agencia: req.params.agency,
-      conta: req.params.account,
-    });
-    res.send(account);
+    const { agency, account } = req.params;
+    const accountFind = await accountModel.find(
+      {
+        agencia: agency,
+        conta: account,
+      },
+      { _id: 0, balance: 1 }
+      /*
+      function (err, bankAcc) {
+        if (err) return handleError(err);
+        console.log(
+          'Agência: %s; Conta: %s; Saldo: %f',
+          agency,
+          account,
+          bankAcc[0].balance
+        );
+      }
+      */
+    );
+
+    if (accountFind.length == 0)
+      res.status(404).send('Agência e/ou conta incorreta(s).');
+
+    res.send(accountFind);
   } catch (error) {
     res.status(500).send(error);
   }
@@ -27,6 +46,25 @@ appRouter.get('/query/:agency/:account', async (req, res) => {
  * Valida se a conta informada existe, retorna um erro se
  * não existir, caso exista retorna o saldo atual da conta.
  */
-appRouter.patch('/deposit/');
+appRouter.patch('/deposit/:agency/:account/:deposit', async (req, res) => {
+  try {
+    const { agency, account, deposit } = req.params;
+
+    const accountFind = await accountModel.findOneAndUpdate(
+      {
+        agencia: agency,
+        conta: account,
+      },
+      { $inc: { balance: deposit } }
+    );
+
+    if (accountFind === null)
+      res.status(404).send('Agência e/ou conta incorreta(s).');
+
+    res.send('Depósito realizado.');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 export { appRouter as bankRouter };
